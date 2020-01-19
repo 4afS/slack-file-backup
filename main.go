834aa4e375ef0c4218409eba19da6ede160568c8
4afS <4afS.h1@gmail.com>
2020-01-19 10:25:05 +0900

    define args

--- main.go
+++ main.go
@@ -1,6 +1,19 @@
 package main
 
-import ()
+import (
+	"flag"
+)
 
 func main() {
+	var (
+		channelID string
+		nfiles    int
+		filetype  string
+	)
+
+	flag.StringVar(&channelID, "c", "", "select channel ID (default all)")
+	flag.IntVar(&nfiles, "n", 100, "get n file names")
+	flag.StringVar(&filetype, "t", "", "download only selected file type (default all)")
+
+	flag.Parse()
 }
